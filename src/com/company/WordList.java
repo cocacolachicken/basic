f516259e847ec4139455d24193946e19e572ad4e
a <tyler.gu@ashbury.ca>
2023-01-11 12:07:25 -0500

    hiiii

---
+++ src/com/company/WordList.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordList {
+    public static ArrayList list = new ArrayList<String>();
+
+    public static void initialize () throws FileNotFoundException {
+
+
+        Scanner scanner = new Scanner(new File("src/valid-wordle-words.txt"));
+
+        while (scanner.hasNextLine()) {
+            list.add(scanner.nextLine());
+        }
+    }
+
+    public static void printWords () {
+        for (int x = 0; x != list.size(); x++) {
+            System.out.println(list.get(x));
+        }
+    }
+}
