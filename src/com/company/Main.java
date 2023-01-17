93003f5b761753a1073d9f636e75c994f467e558
Jlaq88 <john.laquerre@ashbury.ca>
2023-01-17 08:53:20 -0500

    Updated the Main class, imported in button classes and edited thr text box format. Creating a button to use for input of a string from the textbox. I have just added in a scale import too so that I can move the button around rather than it being unappealing at the edge.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.util.Scanner;
-
+import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.transform.Scale;
 
 public class Main extends Application {
 
@@ -17,7 +18,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         // Create a text field
         TextField textField = new TextField();
-        Scanner in = new Scanner(System.in);
 
         // Set prompt text for the text field
         textField.setPromptText("Enter your name");
@@ -28,13 +28,20 @@ public class Main extends Application {
         // Add the text field to the layout
         root.getChildren().add(textField);
 
+
+
+
+
+        Button button = new Button();
+        button.setText("Submit");
+        button.setPrefWidth(300);
+        button.setPrefHeight(100);
+
+        root.getChildren().add(button);
+
+
         // Create a scene and add the layout to it
         Scene scene = new Scene(root, 600, 400);
-
-
-
-
-
 
 
 
@@ -46,9 +53,14 @@ public class Main extends Application {
         // This sets an event so that when the user presses enter the letters they have are converted to a string
         //so that it can be used to solve for best word and it also clears the text box
         textField.setOnAction(e -> {
-             String word = textField.getText();
-             textField.clear();
-             System.out.println(word);
+            String word = textField.getText();
+            textField.clear();
+            System.out.println(word);
+        });
+        button.setOnAction(actionEvent ->  {
+            String word = textField.getText();
+            textField.clear();
+            System.out.println(word);
         });
 
     }
