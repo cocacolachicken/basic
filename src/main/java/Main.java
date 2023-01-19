046c0ac4b62a0ca1e8a860c3bed20bd227ea946a
Jlaq88 <john.laquerre@ashbury.ca>
2023-01-19 14:25:14 -0500

    Updated the Main class, imported in button classes and edited thr text box format. Creating a button to use for input of a string from the textbox. I have just added in a scale import too so that I can move the button around rather than it being unappealing at the edge.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,69 +1,87 @@
 package main.java;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import java.util.Scanner;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
-import javafx.scene.transform.Scale;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
-    public void start(Stage primaryStage) {
-        // Create a text field
+    public void start(Stage stage) {
+        //creating label email
+        Text text = new Text("Please input the letters you have");
+
+
+
+        //Creating Text Filed for email
         TextField textField = new TextField();
-
-        // Set prompt text for the text field
-        textField.setPromptText("Enter your name");
-
-        // Create a VBox layout
-        VBox root = new VBox();
-
-        // Add the text field to the layout
-        root.getChildren().add(textField);
+        textField.setPrefWidth(100);
+        textField.setPrefHeight(50);
 
 
 
-
-
-        Button button = new Button();
-        button.setText("Submit");
-        button.setPrefWidth(300);
+        //Creating Button
+        Button button = new Button("Submit");
+        button.setPrefWidth(400);
         button.setPrefHeight(100);
+        //Creating a Grid Pane
+        GridPane gridPane = new GridPane();
 
-        root.getChildren().add(button);
+        //Setting size for the pane
+        gridPane.setMinSize(600, 400);
+
+        //Setting the padding
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        //Setting the vertical and horizontal gaps between the columns
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+
+        //Setting the Grid alignment
+        gridPane.setAlignment(Pos.CENTER);
+
+        //Arranging all the textboxs and buttons in the grid
+        gridPane.add(text, 1, 0);
+        gridPane.add(textField, 1, 1);
+        gridPane.add(button, 1, 5);
 
 
-        // Create a scene and add the layout to it
-        Scene scene = new Scene(root, 600, 400);
+        //Styling nodes
+        button.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
 
+        text.setStyle("-fx-font: normal bold 20px 'serif' ");
 
+        gridPane.setStyle("-fx-background-color: WHITE;");
 
-        // Set the stage title and scene, and show the stage
-        primaryStage.setTitle("Text Box Example");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Creating a scene object
+        Scene scene = new Scene(gridPane);
 
-        // This sets an event so that when the user presses enter the letters they have are converted to a string
-        //so that it can be used to solve for best word and it also clears the text box
-        textField.setOnAction(e -> {
-            String word = textField.getText();
-            textField.clear();
-            System.out.println(word);
-        });
+        //Setting title to the Stage
+        stage.setTitle("Textbox");
+
+        //Adding scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
         button.setOnAction(actionEvent ->  {
             String word = textField.getText();
             textField.clear();
             System.out.println(word);
         });
 
+
+    }
+
+    public static void main(String args[]){
+
+        launch(args);
     }
 }
+
