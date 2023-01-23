289443ae693721187e78476b8f647b1aec3aca44
a <tyler.gu@ashbury.ca>
2023-01-23 10:43:26 -0500

    dd

--- src/main/java/Guess.java
+++ src/main/java/Guess.java
@@ -29,7 +29,11 @@ public class Guess {
 
     private Letter[] guess = new Letter[5];
 
-    // Code for "stat": g = green, y = yellow, r = red
+    /** Constructs a guess based off of string "str"
+     *
+     * @param str MUST only contain upper/lowercase characters or the special character '?'. else it will throw Exception
+     * @throws Exception Thrown if the string doesn't match the constraints
+     */
     public Guess (String str) throws Exception {
         if (str.length() != 5) {
             throw new Exception ("Please enter in a 5 letter word");
@@ -48,10 +52,26 @@ public class Guess {
         }
     }
 
+    /** Returns a Letter from this guess
+     *
+     * @param x the index of the guess needed
+     * @return the letter requested based off of index x
+     */
     public Letter getGuessLetter (int x) {
         return guess[x];
     }
 
+    /** Processes the list of words, uses inputs based off of guess[] in Filter to process it
+     *
+     * If guess[x] is unknown, it does nothing.
+     * If guess[x] is IN, then it trims down the list of words to a list of words such that all words have letter guess[x] at position x
+     * If guess[x] is ELSEWHERE, then it trims down the list of words to a list of words such that the letter contains letter guess[x] other than words with that letter at position
+     * at a letter where guess[num] is IN or the position of guess[x]
+     *
+     * Note: please use it like "list = guessName.processGuess(list);" because list will get depleted after he usage
+     * @param words the list of words that will be processed according to the
+     * @return the list of words after filtering words based off of this guess
+     */
     public List processGuess (List words) {
         ArrayList l = (ArrayList) words;
         String elsewhere = "";
